33b8b71835edc7fb51ca80b73be4c69d582e2127
Yusef <fix.symbolic.99@gmail.com>
2026-07-31 20:57:07 +0300

    Remove comments from store configuration

--- config/stores.js
+++ config/stores.js
@@ -3,28 +3,28 @@ module.exports = [
     id: 'amazon',
     name: 'Amazon',
     url: 'https://amzn.eu/d/0fnJSx5Z',
-    enabled: true, // ده شغال
+    enabled: true,
     plugin: 'amazon'
   },
   {
     id: 'bestbuy',
     name: 'BestBuy',
     url: 'https://www.bestbuy.com/site/apple-iphone-14-pro-128gb-deep-purple/6509055.p?skuId=6509055',
-    enabled: false, // وقفناه مؤقتاً
+    enabled: false,
     plugin: 'bestbuy'
   },
   {
     id: 'walmart',
     name: 'Walmart',
     url: 'https://www.walmart.com/ip/Apple-iPhone-14-Pro-128GB-Deep-Purple/1750541975',
-    enabled: false, // وقفناه مؤقتاً
+    enabled: false,
     plugin: 'walmart'
   },
   {
     id: 'apple',
     name: 'Apple',
     url: 'https://www.apple.com/shop/buy-iphone/iphone-14-pro',
-    enabled: false, // وقفناه مؤقتاً
+    enabled: false,
     plugin: 'apple'
   }
 ];

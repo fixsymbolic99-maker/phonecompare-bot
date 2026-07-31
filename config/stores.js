a0ef9ce26b9460207d913cc1a2ad0af0bd1a4128
Yusef <fix.symbolic.99@gmail.com>
2026-07-31 20:38:51 +0300

    Disable stores temporarily and add comments
    Updated the enabled status for several stores to false and added comments in Arabic.

--- config/stores.js
+++ config/stores.js
@@ -3,28 +3,28 @@ module.exports = [
     id: 'amazon',
     name: 'Amazon',
     url: 'https://amzn.eu/d/0fnJSx5Z',
-    enabled: true,
+    enabled: true, // ده شغال
     plugin: 'amazon'
   },
   {
     id: 'bestbuy',
     name: 'BestBuy',
     url: 'https://www.bestbuy.com/site/apple-iphone-14-pro-128gb-deep-purple/6509055.p?skuId=6509055',
-    enabled: true,
+    enabled: false, // وقفناه مؤقتاً
     plugin: 'bestbuy'
   },
   {
     id: 'walmart',
     name: 'Walmart',
     url: 'https://www.walmart.com/ip/Apple-iPhone-14-Pro-128GB-Deep-Purple/1750541975',
-    enabled: true,
+    enabled: false, // وقفناه مؤقتاً
     plugin: 'walmart'
   },
   {
     id: 'apple',
     name: 'Apple',
     url: 'https://www.apple.com/shop/buy-iphone/iphone-14-pro',
-    enabled: true,
+    enabled: false, // وقفناه مؤقتاً
     plugin: 'apple'
   }
 ];

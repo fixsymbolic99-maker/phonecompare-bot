8a30c115111ad5eee343b1fa4a4c71fd9c556d7a
Yusef <fix.symbolic.99@gmail.com>
2026-07-31 23:01:24 +0300

    Update store URLs and enable all stores

--- config/stores.js
+++ config/stores.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     id: 'amazon',
     name: 'Amazon',
-    url: 'https://www.amazon.com/dp/B0BDJ2M8KX',
+    url: 'https://amzn.eu/d/0fnJSx5Z',
     enabled: true,
     plugin: 'amazon'
   },
@@ -10,21 +10,21 @@ module.exports = [
     id: 'bestbuy',
     name: 'BestBuy',
     url: 'https://www.bestbuy.com/site/apple-iphone-14-pro-128gb-deep-purple/6509055.p?skuId=6509055',
-    enabled: false,
+    enabled: true,
     plugin: 'bestbuy'
   },
   {
     id: 'walmart',
     name: 'Walmart',
     url: 'https://www.walmart.com/ip/Apple-iPhone-14-Pro-128GB-Deep-Purple/1750541975',
-    enabled: false,
+    enabled: true,
     plugin: 'walmart'
   },
   {
     id: 'apple',
     name: 'Apple',
     url: 'https://www.apple.com/shop/buy-iphone/iphone-14-pro',
-    enabled: false,
+    enabled: true,
     plugin: 'apple'
   }
 ];
